b66585fe9e7fa11e235f4716e239ef1f6d31aad3
psycarlo <psycarlo1@gmail.com>
2026-04-08 18:58:45 +0100

    chore: hermes v1 for ios

--- packages/ui-mobile/src/provider.tsx
+++ packages/ui-mobile/src/provider.tsx
@@ -2,7 +2,7 @@ import {
   InterTight_400Regular,
   InterTight_500Medium,
   InterTight_600SemiBold,
-  InterTight_700Bold,
+  InterTight_700Bold
 } from '@expo-google-fonts/inter-tight'
 import { useFonts } from 'expo-font'
 import { HeroUINativeProvider } from 'heroui-native'
@@ -18,7 +18,7 @@ export const Provider = ({ children }: ProviderProps) => {
     InterTight_400Regular,
     InterTight_500Medium,
     InterTight_600SemiBold,
-    InterTight_700Bold,
+    InterTight_700Bold
   })
 
   if (!fontsLoaded) {
